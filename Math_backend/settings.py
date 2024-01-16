d74b780dbfd10268aa9cf1351f9762a230787368
erlankanyb21 <kanybekov668@gmail.com>
2024-01-16 23:14:47 +0600

    init

--- Math_backend/settings.py
+++ Math_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-n3035!e%m$$$cu#8gle1iocd#o@ittff)9f!&$i0b1+g+qdz63
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://mathapp-529y.onrender.com']
 
 
 # Application definition
