70404f241437d7da7beaef925e93a15ab84eec47
erlankanyb21 <kanybekov668@gmail.com>
2024-01-31 17:38:22 +0600

    fix

--- Math_backend/settings.py
+++ Math_backend/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'Math_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE"))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
