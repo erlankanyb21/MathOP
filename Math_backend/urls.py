332a69f4008a3622afee218313d2e82125de94e8
erlankanyb21 <kanybekov668@gmail.com>
2024-02-22 22:17:10 +0600

    doc

--- Math_backend/urls.py
+++ Math_backend/urls.py
@@ -20,7 +20,6 @@ from math_api.views import MathAPIView, ChatAPIView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
-from .yasg import urlpatterns as doc_urls
 
 
 urlpatterns = [
@@ -31,4 +30,3 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
-urlpatterns += doc_urls
