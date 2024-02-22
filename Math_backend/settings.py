7a597390724e3d47d6eda09cc5b6e83513aa4e21
erlankanyb21 <kanybekov668@gmail.com>
2024-02-22 22:37:24 +0600

    doc

--- Math_backend/settings.py
+++ Math_backend/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['mathapp-4b4m.onrender.com']
 INSTALLED_APPS = [
     'rest_framework',
     'math_api',
+    'drf-yasg',
     'rest_framework_swagger',
     'django.contrib.admin',
     'django.contrib.auth',
